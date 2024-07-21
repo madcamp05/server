77fd7b24603beaa3478a550db4ca61a2b0c8730a
jieunyy <107592904+jieunyy@users.noreply.github.com>
2024-07-21 17:18:05 +0900

    fix: passport 제거

--- server.js
+++ server.js
@@ -30,42 +30,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Passport 로컬 전략 설정
-passport.use(new LocalStrategy(
-  async (username, password, done) => {
-    try {
-      const user = await getUserByUsername(username);
-      if (!user) {
-        return done(null, false, { message: 'Invalid username or password' });
-      }
-
-      const isMatch = await bcrypt.compare(password, user.user_password);
-      if (!isMatch) {
-        return done(null, false, { message: 'Invalid username or password' });
-      }
-
-      return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
-  }
-));
-
-// 세션에서 사용자 정보 직렬화
-passport.serializeUser((user, done) => {
-  done(null, user.user_id);
-});
-
-// 세션에서 사용자 정보 역직렬화
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await getUserById(id);
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
-});
-
 // 라우트 설정
 app.post('/api/login', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
